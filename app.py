0e763df9ee2a3a706e9307a05e939d8e1a09971b
SREENIVASAN-PRASANTH <prasanthshot@gmail.com>
2024-08-12 15:58:58 +0530

    imported model in pickle file format and got output from it and checked the input whether it is spam or not

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import pickle
+import sklearn
+
+cv = pickle.load(open("models/cv.pkl", "rb")) #since cv.pkl file is a binary file
+model = pickle.load(open("models/clf.pkl", "rb"))
 
 app = Flask(__name__)
 
@@ -6,5 +11,20 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/predict", methods = ["POST"])
+def predict():
+    email_text = request.form.get("email-content")
+    tokenized_email = cv.transform([email_text])
+    predictions = model.predict(tokenized_email)
+    
+    if predictions == 1:
+        predictions = 1
+    else:
+        predictions = -1
+    
+    
+    return render_template("index.html", predictions = predictions, email_text = email_text)
+    
+    
 if __name__ == "__main__":
     app.run(debug=True)
